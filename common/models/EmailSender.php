4a20c0f0b1a05cbad8ccf48452ac1b061efab173
igor <bykov.devil@gmail.com>
2018-06-08 17:21:19 +0600

    cron

---
+++ common/models/EmailSender.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 08.06.2018
+ * Time: 9:42
+ */
+
+namespace common\models;
+
+
+use yii\base\Model;
+
+class EmailSender extends Model
+{
+
+}
